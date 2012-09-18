f52fa94590330777918db2ef26a39875d0d9f203
Michael Dennis <michaeljdennis@gmail.com>
2012-09-18 00:27:09 -0700

    Moved options and usage from helper file to README.md

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# CodeIgniter Gravatar Helper
+CodeIgniter Gravatar Helper
+===========================
+A CodeIgniter helper for fetching gravatar images from [gravatar.com](http://gravatar.com).
+
+Options
+----------------
+**Size** - s<br>
+default : 80<br>
+options : 1 - 512
+
+**Default** - d<br>
+default : mm<br>
+options : 404, mm, identicon, monsterid, wavatar, retro
+
+**Force Default** - f<br>
+default : null<br>
+options : y
+
+**Rating** - r<br>
+default : null<br>
+options : g, pg, r, x
+
+Usage
+-------
+	<?php echo gravatar('user@example.com', array('s' => 150, 'd' => 'mm')); ?>
--- gravatar_helper.php
+++ gravatar_helper.php
@@ -1,39 +1,24 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
-* CodeIgniter Gravatar Helper
-*
-* @package      CodeIgniter
-* @subpackage   Helpers
-* @category     Helpers
-* @author       Michael Dennis
-* @version      1.0
-*/
-
-/** 
- * Gravatar
- * 
- * Fetches gravatar images from gravatar.com
- * 
- * @access  public
- * @param   string $email
- * @param   array $options
- * @return  string
- * 
- * Gravatar options
- * s = size (default : 80)
- *     options : 1 - 512
- * d = default (default : mm)
- *     options : 404, mm, identicon, monsterid, wavatar, retro
- * f = forcedefault (default : null)
- *     options : y
- * r = rating (default : null)
- *     options : g, pg, r, x
- * 
- * Example
- * echo gravatar('user@example.com', array('s' => 150))
+ * CodeIgniter Gravatar Helper
+ *
+ * @package      CodeIgniter
+ * @subpackage   Helpers
+ * @category     Helpers
+ * @author       Michael Dennis
+ * @version      1.0
  */
+
 if ( ! function_exists('gravatar'))
 {
+    /**
+     * Get gravatar image
+     *
+     * @access  public
+     * @param   string $email
+     * @param   array $options
+     * @return  string
+     */
     function gravatar($email = '', $options = array())
     {
         $url = isset($_SERVER['HTTPS']) ? 'https://secure.' : 'http://www.';
